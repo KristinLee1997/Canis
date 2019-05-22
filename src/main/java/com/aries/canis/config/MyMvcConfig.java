614af043e45badff968403587a116591a3190434
Kristin <kristinleejava@gmail.com>
2019-05-22 18:57:37 +0800

    :tada: init

---
+++ src/main/java/com/aries/canis/config/MyMvcConfig.java
@@ -0,0 +1,17 @@
+package com.aries.canis.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurationSupport {
+
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+        super.addResourceHandlers(registry);
+    }
+}
