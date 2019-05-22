cb1950e74a59084813c948bb430bc2a863d1b691
Kristin <kristinleejava@gmail.com>
2019-05-22 19:19:55 +0800

    :lipstick: 添加管理员登录界面

--- src/main/java/com/aries/canis/controller/IndexController.java
+++ src/main/java/com/aries/canis/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.aries.canis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +12,15 @@ public class IndexController {
     @ResponseBody
     public String hello() {
         return "Welcome to Canis!";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
